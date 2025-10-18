b9910ef297b76295fcbcc6eac3e23e247f9319e9
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2025-10-18 18:45:53 +0900

    feat: Upgrade 기능 작성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Upgrade
+    private readonly Dictionary<UpgradeType, float> _stats = new();
+
+    public float GetStatValue(UpgradeType type)
     {
-        
+        if (_stats.TryGetValue(type, out float value))
+        {
+            return value;
+        }
+        else
+        {
+            return 0f;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateStat(UpgradeType type, float value)
     {
-        
+        if (_stats.ContainsKey(type))
+        {
+            _stats[type] = value;
+        }
+        else
+        {
+            _stats.Add(type, value);
+        }
+    }
+    #endregion
+
+    #region Money
+    public long _money;
+
+    public long Money
+    {
+        get { return _money; }
+        //set { _money = value; }
+    }
+    #endregion
+    
+    public long useMoney { get; private set; } // 전체 사용 돈
+    
+    public void UpdateMoney(long money)
+    {
+        if (money < 0)
+        {
+            useMoney -= money; // 음수니 빼서 양수로
+        }
+
+        _money += money;
     }
 }
 
