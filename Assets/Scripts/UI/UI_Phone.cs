7f6413a2d861c5a31c24c96441cc7be653b5e3b9
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2025-10-18 16:22:35 +0900

    feat: UI 바인딩

---
+++ Assets/Scripts/UI/UI_Phone.cs
@@ -0,0 +1,78 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Phone : UI_Scene
+{
+    #region enum
+    enum Texts
+    {
+    }
+
+
+    enum Buttons
+    {
+        Btn_Home,
+        Btn_Back,
+        Btn_Icon00,
+        Btn_Icon00_Pressed,
+        Btn_Icon00_Normal,
+        Btn_Icon01,
+        Btn_Icon02,
+        Btn_Icon02_Pressed,
+        Btn_Icon02_Normal,
+    }
+
+
+    enum Images
+    {
+        Img_Phone,
+        Img_SafeAreaBottom,
+        Img_RecentAppsButton,
+    }
+
+
+    enum Objects
+    {
+    }
+
+    #endregion
+
+    Image Img_Phone;
+    Image Img_SafeAreaBottom;
+    Image Img_RecentAppsButton;
+    Button Btn_Home;
+    Button Btn_Back;
+    Button Btn_Icon00;
+    Button Btn_Icon00_Pressed;
+    Button Btn_Icon00_Normal;
+    Button Btn_Icon01;
+    Button Btn_Icon02;
+    Button Btn_Icon02_Pressed;
+    Button Btn_Icon02_Normal;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        BindTexts(typeof(Texts));
+        BindButtons(typeof(Buttons));
+        BindImages(typeof(Images));
+        BindObjects(typeof(Objects));
+
+        Img_Phone = GetImage((int)Images.Img_Phone);
+        Img_SafeAreaBottom = GetImage((int)Images.Img_SafeAreaBottom);
+        Img_RecentAppsButton = GetImage((int)Images.Img_RecentAppsButton);
+        Btn_Home = GetButton((int)Buttons.Btn_Home);
+        Btn_Back = GetButton((int)Buttons.Btn_Back);
+        Btn_Icon00 = GetButton((int)Buttons.Btn_Icon00);
+        Btn_Icon00_Pressed = GetButton((int)Buttons.Btn_Icon00_Pressed);
+        Btn_Icon00_Normal = GetButton((int)Buttons.Btn_Icon00_Normal);
+        Btn_Icon01 = GetButton((int)Buttons.Btn_Icon01);
+        Btn_Icon02 = GetButton((int)Buttons.Btn_Icon02);
+        Btn_Icon02_Pressed = GetButton((int)Buttons.Btn_Icon02_Pressed);
+        Btn_Icon02_Normal = GetButton((int)Buttons.Btn_Icon02_Normal);
+
+    }
+}
---
+++ Assets/Scripts/UI/UI_Title.cs
@@ -0,0 +1,60 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Title : UI_Scene
+{
+    #region enum
+    enum Texts
+    {
+        Txt_Start,
+        Txt_End,
+    }
+
+
+    enum Buttons
+    {
+        Btn_Start,
+        Btn_End,
+    }
+
+
+    enum Images
+    {
+        Img_TitleBG,
+        Img_TitleText,
+    }
+
+
+    enum Objects
+    {
+    }
+
+    #endregion
+
+    Image Img_TitleBG;
+    Button Btn_Start;
+    TextMeshProUGUI Txt_Start;
+    Button Btn_End;
+    TextMeshProUGUI Txt_End;
+    Image Img_TitleText;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        BindTexts(typeof(Texts));
+        BindButtons(typeof(Buttons));
+        BindImages(typeof(Images));
+        BindObjects(typeof(Objects));
+
+        Img_TitleBG = GetImage((int)Images.Img_TitleBG);
+        Btn_Start = GetButton((int)Buttons.Btn_Start);
+        Txt_Start = GetText((int)Texts.Txt_Start);
+        Btn_End = GetButton((int)Buttons.Btn_End);
+        Txt_End = GetText((int)Texts.Txt_End);
+        Img_TitleText = GetImage((int)Images.Img_TitleText);
+
+    }
+}
